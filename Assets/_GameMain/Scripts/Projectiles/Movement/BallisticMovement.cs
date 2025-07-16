1ea85793dd74e225954384d75c56c9e27ff07bf0
LeNa <vladlenfrog@gmail.com>
2025-07-16 23:22:49 +0300

    Add BallisticMovement.cs

---
+++ Assets/_GameMain/Scripts/Projectiles/Movement/BallisticMovement.cs
@@ -0,0 +1,29 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class BallisticMovement : IProjectileMovement
+{
+    private readonly Vector3 m_shootPos;
+    private readonly ITarget m_target;
+
+    public BallisticMovement(Vector3 shootPos, ITarget target)
+    {
+        m_shootPos = shootPos;
+        m_target = target;
+    }
+
+    public async UniTask MoveAsync(Projectile projectileController)
+    {
+        var leadPoint = CoreHelper.GetLeadPoint(
+            m_shootPos, m_target, projectileController.m_speed, maxRange: 100f);
+        
+        var velocity = CoreHelper.CalculateBallisticVelocity(
+            m_shootPos, leadPoint, projectileController.m_speed, Physics.gravity.y);
+
+        projectileController.SetPhysicsEnabled(true, velocity);
+        while (projectileController && projectileController.gameObject.activeSelf)
+            await UniTask.Yield();
+    }
+    
+   
+}
